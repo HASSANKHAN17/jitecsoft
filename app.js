1a0dfd904f4e753d86bc8e06ce7f0ea80a1ea78e
Mohammed Hassan Khan <54667683+HASSANKHAN17@users.noreply.github.com>
2021-10-28 00:07:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,9 @@ app.use(cors())
 
 
 
-
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static("client/build"))
+ app.use(express.static("client/build"))
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))   
 }
 
 app.listen(process.env.PORT|| 3002,(req,res)=>{
