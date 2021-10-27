e329133b29919e3bde9432ca98d0d2e214ba9792
sharjeel shaikh <sharjeelarts@gmail.com>
2021-10-27 21:57:40 +0530

    added

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const bodyParser=require('body-parser')
+const cors = require('cors')
+
+
+const app = express()
+app.use(cors())
+
+
+
+
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static("client/build"))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
+
+app.listen(process.env.PORT|| 3002,(req,res)=>{
+console.log("server started on");
+})
